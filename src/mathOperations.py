b1baa27f89219da96e871eae993d1bb829b098cf
ArturoITA <55923813+ArturoITA@users.noreply.github.com>
2019-09-28 19:56:13 -0500

    Initial commit

---
+++ src/mathOperations.py
@@ -0,0 +1,30 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+a = 2
+b=3
+
+c = a+b
+
+c = b-c
+
+b = "arturo"
+c = "is awesome"
+
+d = b + " " + c 
+print(d)
+
+d = b + " is cool"
+print(d)
+
+x = 2
+y = 3
+
+z = x * y 
+print (z)
+z = x / y 
+print (z)
+z = x // y
+print (z)
---
+++ src/mathOperatorDrills.py
@@ -0,0 +1,50 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+f = "Mario"
+g = " Kart"
+h = " Mobile"
+
+j = f + g + h
+print(j)
+
+a = 1 + 2
+print(a)
+b = a + 1
+print(b)
+
+a = 1 + 2 
+print(a)
+b = a - 1
+print(b)
+
+a = 1 * 2
+c = 2
+print(a)
+print(c)
+b = a * c
+print(b)
+
+a = 1 / 2 
+c = 3 / 6
+print(a)
+print(c)
+b = a / c
+print(b)
+
+a = 1 // 2 
+c = 3 // 6
+print(a)
+print(c)
+b = a // c
+print(b)
+
+a = 1 % 2 
+c = 3 % 6
+print(a)
+print(c)
+b = a % c
+print(b)
