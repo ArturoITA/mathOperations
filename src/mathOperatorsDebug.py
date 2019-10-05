a5c3ebc4d92fce0a55eb9696a74e0a4c550e3a80
ArturoITA <55923813+ArturoITA@users.noreply.github.com>
2019-10-05 10:27:22 -0500

    Update mathOperatorsDebug.py

--- src/mathOperatorsDebug.py
+++ src/mathOperatorsDebug.py
@@ -3,3 +3,17 @@ Created on Sep 28, 2019
 
 @author: ITAUser
 '''
+
+40 * 0 
+  
+"Hello and" 
+   
+6 * 5 + 1  
+
+print(10/2)       
+  
+print("LOL" * 3)         
+
+print( "1" + "2" + "3")    
+
+print(10 % 2 == 0)
